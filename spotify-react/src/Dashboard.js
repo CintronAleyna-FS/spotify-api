8f8b7fdb70719acdae22b758d103cae8d46cb247
CintronAleyna-FS <88294142+CintronAleyna-FS@users.noreply.github.com>
2022-08-31 23:34:26 -0500

    results

--- spotify-react/src/Dashboard.js
+++ spotify-react/src/Dashboard.js
@@ -2,8 +2,6 @@ import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import MyNav from './components/MyNav'
 import NoResults from './NoResults'
-import Square from './images/square.png'
-
 
 import useAuth from './useAuth';
 
@@ -14,7 +12,6 @@ function Dashboard({code}) {
     const [artists, setArtists] = useState([])
     const [playlists, setPlaylists] = useState([])
     const [shows, setShows] = useState([])    
-    const [artistName, setArtistName] = useState({name: "", url: ""})
     
 
     const handleKeyPress = (e) => {
@@ -55,68 +52,72 @@ function Dashboard({code}) {
                     <NoResults></NoResults>
                 : (
                     <div className="text-center flex flex-col items-center p-2 mt-16 space-y-12 ">
-                    <div className="flex flex-row space-x-12 p-2">
-                        <p className="underline mt-auto mb-auto text-2xl mr-24">Artists ➜</p>
-                        {/* {artists.map(artist =>{
+                        <div className="flex flex-row space-x-12 p-2">
+                            <p className="underline mt-auto mb-auto text-2xl mr-24">Artists ➜</p>
+                            {artists.map(artist =>{
+                                return(
+                                    <div>
+                                        <a href={artist.external_urls.spotify}>
+                                            <div style={{backgroundImage: `url(${artist.images[0].url})`, 
+                                                height: '100%',
+                                                backgroundRepeat: 'no-repeat',
+                                                backgroundPosition: 'center',
+                                                backgroundSize: 'cover',
+                                                display: 'grid',
+                                                placeItems: 'center',
+                                                height: '16rem',
+                                                width: '16rem'}} className='h-full'>
+                                                <p className="text-lg">{artist.name}</p>
+                                            </div>
+                                        </a> 
+                                    </div>
+                                )
+                            })}
+                        </div>
+                        <div className="flex flex-row space-x-12 p-2">
+                        <p className="underline mt-auto mb-auto text-2xl mr-24">Playlists ➜</p>
+                        {playlists.map(playlist =>{
                             return(
                                 <div>
-                                    <a href={artist.href}>
-                                        <div style={{backgroundImage: `url(${artist.images[0].url})`}, styles.image}className='h-full'>
-                                            <p className="text-lg">{artist.name}</p>
+                                    <a href={playlist.external_urls.spotify}>
+                                        <div style={{backgroundImage: `url(${playlist.images[0].url})`, 
+                                            height: '100%',
+                                            backgroundRepeat: 'no-repeat',
+                                            backgroundPosition: 'center',
+                                            backgroundSize: 'cover',
+                                            display: 'grid',
+                                            placeItems: 'center',
+                                            height: '16rem',
+                                            width: '16rem'}} className='h-full'>
+                                            <p className="text-lg">{playlist.name}</p>
                                         </div>
                                     </a> 
                                 </div>
                             )
-                        })} */}
-                    </div>
-                    <div className="flex flex-row space-x-12 p-2">
-                        <p className="underline mt-auto mb-auto text-2xl mr-24">Playlists ➜</p>
-                        <div>
-                            <a href="">
-                                <div style={{backgroundImage: `url(${Square})`}, styles.image}className='h-full'>
-                                    <p className="text-lg">ka</p>
-                                </div>
-                            </a> 
+                        })}
                         </div>
-                        <div>
-                            <a href="">
-                                <div style={{backgroundImage: `url(${Square})`}, styles.image}className='h-full'>
-                                    <p className="text-lg">ka</p>
-                                </div>
-                            </a> 
-                        </div>
-                        <div>
-                            <a href="">
-                                <div style={{backgroundImage: `url(${Square})`}, styles.image}className='h-full'>
-                                    <p className="text-lg">ka</p>
-                                </div>
-                            </a> 
-                        </div>
-                    </div>
-                    <div className="flex flex-row space-x-12 p-2">
+                        <div className="flex flex-row space-x-12 p-2">
                         <p className="underline mt-auto mb-auto text-2xl mr-24">Shows ➜</p>
-                        <div>
-                            <a href="">
-                                <div style={{backgroundImage: `url(${Square})`}, styles.image}className='h-full'>
-                                    <p className="text-lg">ka</p>
+                        {shows.map(show =>{
+                            return(
+                                <div>
+                                    <a href={show.external_urls.spotify}>
+                                        <div style={{backgroundImage: `url(${show.images[0].url})`, 
+                                            height: '100%',
+                                            backgroundRepeat: 'no-repeat',
+                                            backgroundPosition: 'center',
+                                            backgroundSize: 'cover',
+                                            display: 'grid',
+                                            placeItems: 'center',
+                                            height: '16rem',
+                                            width: '16rem'}} className='h-full'>
+                                            <p className="text-lg">{show.name}</p>
+                                        </div>
+                                    </a> 
                                 </div>
-                            </a> 
+                            )
+                        })}
                         </div>
-                        <div>
-                            <a href="">
-                                <div style={{backgroundImage: `url(${Square})`}, styles.image}className='h-full'>
-                                    <p className="text-lg">ka</p>
-                                </div>
-                            </a> 
-                        </div>
-                        <div>
-                            <a href="">
-                                <div style={{backgroundImage: `url(${Square})`}, styles.image}className='h-full'>
-                                    <p className="text-lg">ka</p>
-                                </div>
-                            </a> 
-                        </div>
-                    </div>
                 </div> 
                 ) 
             }
@@ -128,7 +129,6 @@ export default Dashboard;
 
 const styles = {
     image: {
-        backgroundImage: `url(${Square})`,
         height: '100%',
         backgroundRepeat: 'no-repeat',
         backgroundPosition: 'center',
